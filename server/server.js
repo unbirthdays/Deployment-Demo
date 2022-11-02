803d9b9ee34190db2bb57b549c4c707c9a5f5f52
Student Sab <studentsab@Sabrinas-MBP.attlocal.net>
2022-11-02 00:27:19 -0500

    port setup

--- server/server.js
+++ server/server.js
@@ -5,3 +5,14 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../index.html")); // dirname is just a variable, .. for index.html because we need to go up a level to access it
+});
+
+const port = process.env.PORT || 4005; // default to PORT 4005 for local deployment before client need
+
+app.listen(port, () => {
+    console.log(`We vibin on port ${port}`);
+});
+
