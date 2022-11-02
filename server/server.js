a681128fc6ed9346589edb3dbdecf4f4377c125f
Student Sab <studentsab@Sabrinas-MBP.attlocal.net>
2022-11-02 01:25:22 -0500

    trying out a fix for blue text

--- server/server.js
+++ server/server.js
@@ -6,8 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html")); // dirname is just a variable, .. for index.html because we need to go up a level to access it
+app.get("/", function(req, res) { // / is just the home page
+    res.sendFile(path.join(__dirname, "../public/index.html")); // dirname is just a variable, .. for index.html because we need to go up a level to access it
+});
+
+app.get("/styles", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.css"));
 });
 
 const port = process.env.PORT || 4005; // default to PORT 4005 for local deployment before client need
